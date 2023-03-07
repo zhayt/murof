a1e7f4cb617b469af268042b3be62f9e2ed17835
Aybek <turk.883ar@gmail.com>
2023-03-07 04:45:43 -0800

    migrations

---
+++ migrations/20230306_init.down.sql
@@ -0,0 +1,15 @@
+DROP TABLE marked_comment;
+
+DROP TABLE comment;
+
+DROP TABLE post_category;
+
+DROP TABLE category;
+
+DROP TABLE marked_post;
+
+DROP TABLE post;
+
+DROP TABLE session;
+
+DROP TABLE user;
