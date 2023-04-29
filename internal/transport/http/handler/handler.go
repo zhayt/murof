7f6f88859b4c8e2b3644aacec6b6afadcd1f4590
Aybek <turk.883ar@gmail.com>
2023-04-28 22:19:31 -0700

    add: handler

--- internal/transport/http/handler/handler.go
+++ internal/transport/http/handler/handler.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Handler struct {
+}
+
+type ErrorResponse struct {
+	Message string
+	Code    int
+}
+
+func errorResponse(w http.ResponseWriter, message string, status int) {
+	response := ErrorResponse{Message: message, Code: status}
+	html, err := template.ParseFiles("./ui/error.html")
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if err = html.Execute(w, response); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+}
